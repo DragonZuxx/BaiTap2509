2b8112f616af886c37d1ef4ae7a11aed558e73e6
DragonZuxx <vunlhe170236@fpt.edu.vn>
2024-09-30 20:23:38 +0700

    Done

--- Q1.py
+++ Q1.py
@@ -5,13 +5,20 @@ import time
 set1 = set(random.sample(range(1, 10000000), 2000000))
 set2 = set(random.sample(range(1, 10000000), 2000000))
 
+# Đảm bảo ít nhất 5% phần tử giao nhau
 set2.update(random.sample(list(set1), 100000))
 
-#Tính tổng thời gian tìm tập giao và hợp
+# Tính tổng thời gian tìm tập giao và hợp
 start_time = time.time()
-intersection = set1 & set2
-union = set1 | set2
+intersection = set1 & set2  # Tìm tập giao
+union = set1 | set2         # Tìm tập hợp
 end_time = time.time()
 
-print(f"Thời gian tính toán: {end_time - start_time} giây")
+# Đếm số phần tử giao nhau và hợp
+num_intersections = len(intersection)
+num_union = len(union)
 
+
+print(f"Số phần tử giao nhau: {num_intersections}")
+print(f"Số phần tử hợp: {num_union}")
+print(f"Thời gian tính toán: {end_time - start_time} giây")
